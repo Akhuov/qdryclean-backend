738f010f6215bbc4731f4752a8e349e8152c0562
ohuno <ohuno@DEFAULT>
2026-02-02 17:55:39 +0500

    Обновлён UpdateChargeCommand: отказ от наследования ChargeDto

--- QDryCleaning.Application/UseCases/charges/Commands/UpdateChargeCommand.cs
+++ QDryCleaning.Application/UseCases/charges/Commands/UpdateChargeCommand.cs
@@ -1,8 +1,15 @@
 ﻿using MediatR;
 using QDryClean.Application.Common.Responses;
 using QDryClean.Application.Dtos;
+using System.Text.Json.Serialization;
 
 namespace QDryClean.Application.UseCases.Charges.Commands
 {
-    public class UpdateChargeCommand : ChargeDto, IRequest<ApiResponse<ChargeDto>> { }
+    public class UpdateChargeCommand : IRequest<ApiResponse<ChargeDto>> 
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public decimal Cost { get; set; }
+        public string? Name { get; set; }
+    }
 }
